cdbbb901db2d03c0c7a53de761ff575d0b5970e8
Aditya Thorat <thorat.aditya@outlook.com>
2022-06-03 18:30:50 +0530

    complete python-weather-visualisation

--- app.py
+++ app.py
@@ -2,25 +2,91 @@
 # localhost:8501
 
 import os
-import pytz
-import pyowm
+import requests
 import streamlit as st
-from matplotlib import dates
-from datetime import datetime
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import matplotlib
+import tkinter
+import numpy
 
-# API key
-owm=pyowm.OWM('your-api-key')
-mgr=owm.weather_manager()
 
-st.title("5 Day Weather Forecast")
-st.write("### Write the name of a City and select the Temperature Unit and Graph Type from the sidebar")
+matplotlib.use('TkAgg')
 
-place=st.text_input("NAME OF THE CITY :", "")
 
-if place == None:
-    st.write("Input a CITY!")
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
 
-unit=st.selectbox("Select Temperature Unit",("Celsius","Fahrenheit"))
 
-g_type=st.selectbox("Select Graph Type",("Line Graph","Bar Graph"))
+def getcurrloc():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    return response.get("region")
+
+
+def getapidata(place):
+    api_key = os.environ['openweathermap_apikey']
+    api_link = "https://api.openweathermap.org/data/2.5/forecast?q=" + place + "&appid=" + api_key
+    api_data = requests.get(api_link)
+    data = api_data.json()
+    return data
+
+
+def getfivedays():
+    days = {}
+    a = datetime.today()
+    for x in range (0, 5):
+        days[x] =  str((a + timedelta(days = x)).strftime("%d %b %Y"))
+    return days
+
+
+def linegraph(place, days, temp_min, temp_max):
+    plt.plot(days.values(), temp_min, linewidth=2)
+    plt.plot(days.values(), temp_max, linewidth=2)
+    plt.title(place)
+    plt.show()
+
+
+def bargraph(place, days, temp_min, temp_max):
+    width = 2
+    dayst = days.keys()
+    plt.bar(dayst, temp_min, width, label='Min Temp')
+    plt.bar(dayst + width, temp_max, width, label='Max Temp')
+    plt.xticks(dayst + width / 2, days.values())
+    plt.legend(loc='best')
+    plt.title(place)
+    plt.show()
+
+
+def main():
+
+    st.title("5 Day Weather Forecast")
+    st.write("### Enter Location name and select Graph type from dropdown")
+    place=st.text_input("Type city name here:", getcurrloc())
+    g_type = st.selectbox("Choose graph type", ("Line Graph", "Bar Graph"))
+
+    data = getapidata(place)
+
+    if(data['cod'] == '200'):
+        temp_min = []
+        temp_max = []
+        dayst = []
+
+        for i in range(0, data['cnt'], 8):
+            temp_min.append(data['list'][i+5]['main']['temp_min'] - 273.17)
+            temp_max.append(data['list'][i+1]['main']['temp_max'] - 273.17)
+            dayst.append(i)
+
+        days = getfivedays()
+
+        if(g_type == "Line Graph"):
+            linegraph(place, days, temp_min, temp_max)
+        elif(g_type == "Bar Graph"):
+            bargraph(place, days, temp_min, temp_max)
+        
+    else:
+        print(data['message'])
+
+if __name__ == "__main__":
+    main()
